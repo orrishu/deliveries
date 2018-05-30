767c565a144ee7ea24cfed4183e199aa4f78c4ec
Ori Shuchami <ori_s@ifat.com>
2018-05-30 23:44:49 +0300

    fixed design and click on row on DeliveryItem.

--- src/components/Deliveries/DeliveryItem.js
+++ src/components/Deliveries/DeliveryItem.js
@@ -52,20 +52,20 @@ export default class DeliveryItem extends Component {
     const style = ix % 2 == 0 ? 'row' : 'alt-row'
     return (
       <div>
-        <Row className="show-grid" styleName={style} onClick={this.openBig}>
-          <Col xs={1} md={1}>
+        <Row className="show-grid" styleName={style}>
+          <Col xs={1} md={1} onClick={this.openBig}>
             <div>{`#${delivery.Id}`}</div>
           </Col>
-          <Col xs={2} md={2}>
+          <Col xs={2} md={2} onClick={this.openBig}>
             <div style={{direction: 'ltr'}}>{delivery.ReceivedAt}</div>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={1} md={1} onClick={this.openBig}>
             <div>{delivery.FromAddress}</div>
           </Col>
-          <Col xs={2} md={2}>
+          <Col xs={2} md={2} onClick={this.openBig}>
             <div>{delivery.ToAddress}</div>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={1} md={1} onClick={this.openBig}>
             <div>{delivery.Importance}</div>
           </Col>
           <Col xs={2} md={2}>
@@ -112,44 +112,27 @@ export default class DeliveryItem extends Component {
               />
             </div>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={1} md={1} onClick={this.openBig}>
             <div>{delivery.Status}</div>
           </Col>
-          {/*<Col xs={6} md={3} style={{paddingBottom: '8px'}}>
-            <div style={{padding: 0}}>{
-              <DropdownButton
-                bsStyle="default"
-                bsSize="xsmall"
-                title="choose"
-                id={`dropdown-basic-${ix}`}
-              >
-                {
-                  employees.map((employee, index) =>
-                    <MenuItem key={index} eventKey={index} onClick={() => this.onClick(employee.id)}>{employee.name}</MenuItem>, this)
-                }
-              </DropdownButton>
-            }</div>
-          </Col>*/}
         </Row>
         <Row className="show-grid" style={show}>
-          <Col xs={12} md={12}>
-            <Row>
-              <Col xs={6} md={6} style={{lineHeight: '2'}}>
-                <div>{t('deliveries.from')}: {delivery.From}</div>
-                <div>{t('deliveries.to')}: {delivery.To}</div>
-                <div>{t('deliveries.deliveryNote')}: {delivery.DeliveryNote}</div>
-                <div>{t('deliveries.name1')}: {delivery.Name1}</div>
-                <div>{t('deliveries.name2')}: {delivery.Name2}</div>
-              </Col>
-              <Col xs={6} md={6} style={{lineHeight: '2'}}>
-                <div>{t('deliveries.date')}: {delivery.Date}</div>
-                <div>{t('deliveries.description')}: {delivery.Description}</div>
-                <div>{t('deliveries.combo')}: {delivery.Combo1}</div>
-                <div>{t('deliveries.reciever1')}: {delivery.Reciever1}</div>
-                <div>{t('deliveries.collect')}: {delivery.Collect}</div>
-              </Col>
-            </Row>
+
+          <Col xs={6} md={6} style={{lineHeight: '2'}}>
+            <div>{t('deliveries.from')}: {delivery.From}</div>
+            <div>{t('deliveries.to')}: {delivery.To}</div>
+            <div>{t('deliveries.deliveryNote')}: {delivery.DeliveryNote}</div>
+            <div>{t('deliveries.name1')}: {delivery.Name1}</div>
+            <div>{t('deliveries.name2')}: {delivery.Name2}</div>
           </Col>
+          <Col xs={6} md={6} style={{lineHeight: '2'}}>
+            <div>{t('deliveries.date')}: {delivery.Date}</div>
+            <div>{t('deliveries.description')}: {delivery.Description}</div>
+            <div>{t('deliveries.combo')}: {delivery.Combo1}</div>
+            <div>{t('deliveries.reciever1')}: {delivery.Reciever1}</div>
+            <div>{t('deliveries.collect')}: {delivery.Collect}</div>
+          </Col>
+
         </Row>
       </div>
     )
